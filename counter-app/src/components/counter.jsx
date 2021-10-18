fcdfebb816b108280e21826c9bc885945bdb62c4
navnit0707 <knavnit81@gmail.com>
2021-10-18 18:57:03 +0530

    rendering lists

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -4,7 +4,8 @@ class Counter extends Component {
     //state is a property of component 
     // this stores data that component needed
     state = {
-        count: 0
+        count: 0,
+        tags: ["tag1","tag2","tag3"]
     };
     /*style = {
         fontSize: 10,
@@ -15,9 +16,11 @@ class Counter extends Component {
         <div>
             <span  className={this.getBadgeClass()}>{this.formatCount()}</span>
             <button className="btn btn-secondary btn-sm">Increment </button>
+            <ul>{this.state.tags.map(tag=> <li key={tag}>{tag}</li>)}</ul>
         </div>
         );
     }
+    //rendering class dynamicaly 
     getBadgeClass(){
         let classes = "badge m-2 badge-";
         classes += this.state.count === 0 ? "warning" : "primary";
