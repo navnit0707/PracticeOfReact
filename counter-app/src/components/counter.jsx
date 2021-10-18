b52aa27e39357ed2ac2155565f987d94040f1d8e
navnit0707 <knavnit81@gmail.com>
2021-10-18 19:14:13 +0530

    empty list

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -5,30 +5,22 @@ class Counter extends Component {
     // this stores data that component needed
     state = {
         count: 0,
-        tags: ["tag1","tag2","tag3"]
+        tags: []
     };
-    /*style = {
-        fontSize: 10,
-        fontWeight: "Bold"
-    }; */
+
+    renderTags(){
+        if(this.state.tags.length === 0) return <p>There are no tags!</p>;
+
+        return <ul>{this.state.tags.map(tag=> <li key={tag}>{tag}</li>)}</ul> ;
+    }
     render() {   
+
         return (
         <div>
-            <span  className={this.getBadgeClass()}>{this.formatCount()}</span>
-            <button className="btn btn-secondary btn-sm">Increment </button>
-            <ul>{this.state.tags.map(tag=> <li key={tag}>{tag}</li>)}</ul>
+            {this.state.tags.length === 0 && "Please create a new tag!"}
+            {this.renderTags()}
         </div>
         );
-    }
-    //rendering class dynamicaly 
-    getBadgeClass(){
-        let classes = "badge m-2 badge-";
-        classes += this.state.count === 0 ? "warning" : "primary";
-        return classes;
-    }
-    formatCount(){
-        const {count} = this.state; //object destructuring
-        return count === 0 ? "Zero" : count;
     }
 }
  
