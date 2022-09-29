46b7e83e2e8a997645c2b1cc554f61e46420d907
navnit0707 <knavnit81@gmail.com>
2022-09-29 20:55:22 +0530

    addded store and dispatch actions

--- redux/src/redux.js
+++ redux/src/redux.js
@@ -1,1 +1,43 @@
-console.log("App.js is running ");
+import {createStore} from 'redux';
+
+const store = createStore((state ={count: 0}, action)=>{
+
+    switch (action.type){
+        case 'INCREMENT':
+            return{
+                count: state.count +1
+            };
+        case 'DECREMENT':
+            return{
+                count: state.count -1
+            };
+        case 'RESET':
+            return{
+                count: 0
+            };
+        default:
+            return state;
+    }
+});
+
+//Action - than an object that gets sent to the state
+// increment 
+store.dispatch({
+    type: 'INCREMENT'
+});
+store.dispatch({
+    type: 'INCREMENT'
+});
+
+store.dispatch({
+    type: 'DECREMENT'
+});
+store.dispatch({
+    type: 'RESET'
+});
+store.dispatch({
+    type: 'DECREMENT'
+});
+
+//
+console.log(store.getState());
